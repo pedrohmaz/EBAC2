6c60db634a8ef725b8f41fe88823517296c918be
Pedro H Mazzilli <pedrohmaz@gmail.com>
2024-09-26 15:17:44 -0300

    Layouts Parte 1

---
+++ app/src/main/java/com/example/ebac2/LayoutsParte1.kt
@@ -0,0 +1,15 @@
+package com.example.ebac2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LayoutsParte1: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.layouts_parte_1)
+
+    }
+}
